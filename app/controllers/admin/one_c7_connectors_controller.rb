d736ce9285fd8da98c0259430e319b6425e7a960
Michail A. Pospelov <mixan946@yandex.ru>
2012-08-08 12:00:35 +0400

    finish parse

--- app/controllers/admin/one_c7_connectors_controller.rb
+++ app/controllers/admin/one_c7_connectors_controller.rb
@@ -41,7 +41,7 @@ class Admin::OneC7ConnectorsController < Admin::BaseController
             end
 
             parse_products(xml.elements.first.elements[2].elements[5])
-            parse_products_with_prices(offers_xml.css("Предложения"))
+           # parse_products_with_prices(offers_xml.css("Предложение"))
 
             set_product_price
 
@@ -60,46 +60,35 @@ class Admin::OneC7ConnectorsController < Admin::BaseController
 
     def set_product_price
         Product.all.each do |product|
-            variant = product.variant
-            variant.price = product.variants.second.price
-            variant.cost_price = product.variants.second.cost_price
-            debugger
+            if not product.variants.blank?
+                price = 0
+                product.variants.each do |var|
+                    price = var.price if var.price.to_i != 0
+                end
+                product.price = price
+                product.save
+            end
         end
     end
     def parse_products_with_prices(products)
-        products.children.each do |xml_product|
+        products.each do |xml_product|
+            product = Product.find_by_code_1c(xml_product.css("Ид").text.split('#').first)
 
-            unique_flag = true
-            product = Product.find_by_code_1c(xml_product.elements[1].text.split('#').first)
-            if xml_product.elements[2].expanded_name == "Штрихкод"
-                magic_number = 1
-            else
-                magic_number = 0
-            end
-
-            if xml_product.elements[4 + magic_number].expanded_name == "Статус"
-                magic_number=magic_number+1
-            end
-
-            variant = Variant.find_or_initialize_by_code_1c(xml_product.elements[1].text)
-            puts xml_product.elements[1].text
+            variant = Variant.find_or_initialize_by_code_1c(xml_product.css("Ид").text)
+            puts xml_product.css("Ид").text
             variant.product_id = product.id
-            variant.price = xml_product.elements[4 + magic_number].elements[1].elements[3].text
-            variant.cost_price =xml_product.elements[4 + magic_number].elements[1].elements[3].text
-            xml_product.elements.each do |el|
-                if el.expanded_name == "Количество"
-                variant.count_on_hand =xml_product.elements[5 + magic_number ].text
-                end
-            end
-            xml_product.elements[3 + magic_number ].elements.each do |option|
-                if ProductOptionType.where(:product_id => product.id, :option_type_id => OptionType.find_by_name(option.elements[1].text).id).blank?
-                    product_option_type = ProductOptionType.new(:product => product, :option_type => OptionType.find_by_name(option.elements[1].text))
+            variant.price = xml_product.css("ЦенаЗаЕдиницу").text
+            variant.cost_price =xml_product.css("ЦенаЗаЕдиницу").text
+            variant.count_on_hand = xml_product.css("Количество").text if not xml_product.css("Количество").text.blank?
+            xml_product.css("ХарактеристикаТовара").each do |option|
+                if ProductOptionType.where(:product_id => product.id, :option_type_id => OptionType.find_by_name(option.css("Наименование").text).id).blank?
+                    product_option_type = ProductOptionType.new(:product => product, :option_type => OptionType.find_by_name(option.css("Наименование").text))
                     product_option_type.save
                 end
-                if OptionValue.find_by_name(option.elements[2].text)
-                    option_value = OptionValue.find_by_name(option.elements[2].text)
+                if OptionValue.find_by_name(option.css("Значение").text)
+                    option_value = OptionValue.find_by_name(option.css("Значение").text)
                 else
-                    option_value = OptionValue.create(:option_type_id => OptionType.find_by_name(option.elements[1].text), :name => option.elements[2].text,:presentation => option.elements[2].text)
+                    option_value = OptionValue.create(:option_type_id => OptionType.find_by_name(option.css("Наименование").text), :name => option.css("Значение").text,:presentation => option.css("Значение").text)
                 end
                 variant.option_values << option_value
             end
