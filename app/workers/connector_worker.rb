ffecf0b727636075b9ff9b8eb5c26664282990df
Michail A. Pospelov <mixan946@yandex.ru>
2012-08-17 01:12:00 +0400

    rail fix

--- app/workers/connector_worker.rb
+++ app/workers/connector_worker.rb
@@ -1,7 +1,7 @@
 class ConnectorWorker
   include Sidekiq::Worker
   def perform
-      Synergy1c7Connector::Connection.new.parse_xml("#{Rail.root}/../../shared")
+      Synergy1c7Connector::Connection.new.parse_xml("#{Rails.root}/../../shared")
   end
 end
 
