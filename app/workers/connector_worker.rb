7156789901eec9c00f1d62122df1eb10e90f2f40
Michail A. Pospelov <mixan946@yandex.ru>
2012-08-17 11:31:37 +0400

    folder changes

--- app/workers/connector_worker.rb
+++ app/workers/connector_worker.rb
@@ -1,7 +1,7 @@
 class ConnectorWorker
   include Sidekiq::Worker
   def perform
-      Synergy1c7Connector::Connection.new.parse_xml("#{Rails.root}/../../shared")
+      Synergy1c7Connector::Connection.new.parse_xml
   end
 end
 
