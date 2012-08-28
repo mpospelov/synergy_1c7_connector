1adb8c96f84e7e670c7b49bdbd934d3cafc84290
Michail A. Pospelov <mixan946@yandex.ru>
2012-08-28 17:48:32 +0400

    string fix

--- app/workers/connector_worker.rb
+++ app/workers/connector_worker.rb
@@ -1,7 +1,7 @@
 class ConnectorWorker
   include Sidekiq::Worker
   def perform
-      FtpSynch::Get.new.dowload_dir(Rails.root)
+      FtpSynch::Get.new.dowload_dir(Rails.root.to_s)
       Synergy1c7Connector::Connection.new.parse_xml
   end
 end
