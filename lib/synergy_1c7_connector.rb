bc0eddcb7c7938f13f7b4ec599ff844f57a6d9ce
Michail A. Pospelov <mixan946@yandex.ru>
2012-08-30 17:58:28 +0400

    return xml_string

--- lib/synergy_1c7_connector.rb
+++ lib/synergy_1c7_connector.rb
@@ -32,6 +32,10 @@ module Synergy1c7Connector
             return @@instance
         end
 
+        def xml_string
+            return @xml_string
+        end
+
         def self.reset_xml_var
             @@instance = Connection.new
             @xml_string = "<?xml version=\"1.0\" encoding=\"windows-1251\"?><КоммерческаяИнформация ВерсияСхемы=\"2.03\" ДатаФормирования=\"#{Time.now.to_s.split(" ").first.tr(".","-")} \">"
