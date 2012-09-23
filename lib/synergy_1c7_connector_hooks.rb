5ee76e4b9939b8ffb451cc79a85647817a365953
Michail A. Pospelov <mixan946@yandex.ru>
2012-09-23 14:23:08 +0400

    last hook fix

--- lib/synergy_1c7_connector_hooks.rb
+++ lib/synergy_1c7_connector_hooks.rb
@@ -3,12 +3,8 @@ class Synergy1c7ConnectorHooks < Spree::ThemeSupport::HookListener
 
   insert_after :admin_inside_taxon_form, "admin/shared/code_1c_fields"
 
-  insert_after :admin_configurations_menu do
-    "<%= configurations_menu_item(I18n.t('one_c7_connector'), new_admin_one_c7_connector_path, I18n.t('one_c7_connector_desc')) %>"
-  end
-
   insert_after :admin_configurations_sidebar_menu do
-    %(<li<%== ' class="active"' if controller.controller_name == 'one_c7_connectors' %>><%= link_to t("one_c7_connector"), new_admin_one_c7_connector_path %></li>)
+    %(<li<%== ' class="active"' if controller.controller_name == 'one_c7_connectors' %>><%= link_to t("one_c7_connector"), admin_one_c7_connector_path %></li>)
   end
    insert_after :admin_tabs do
       %(<%= tab(:one_c7_connector) %>)
