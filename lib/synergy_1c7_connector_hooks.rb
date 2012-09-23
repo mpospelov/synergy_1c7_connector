74190995170a1f9d6fa8ecad9387343f6f624249
Michail A. Pospelov <mixan946@yandex.ru>
2012-09-23 13:48:54 +0400

    fix

--- lib/synergy_1c7_connector_hooks.rb
+++ lib/synergy_1c7_connector_hooks.rb
@@ -10,7 +10,7 @@ class Synergy1c7ConnectorHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_configurations_sidebar_menu do
     %(<li<%== ' class="active"' if controller.controller_name == 'one_c7_connectors' %>><%= link_to t("one_c7_connector"), new_admin_one_c7_connector_path %></li>)
   end
-  insert_before :admin_dashboard do
-     %( <p class="form-buttons"><%= link_to t("one_c7_connector"), new_admin_one_c7_connector_path %></p>)
+   insert_after :admin_tabs do
+      %(<%= tab(:synergy_1c7_connector) %>)
   end
 end
