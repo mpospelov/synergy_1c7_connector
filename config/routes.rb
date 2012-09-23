89bbb58e021af2aee42c130fdeed4a7847563499
Michail A. Pospelov <mixan946@yandex.ru>
2012-09-23 14:16:37 +0400

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resource :one_c7_connector, :only => [:index, :create]
+    resource :one_c7_connector, :only => [:show, :create]
   end
   match 'admin/1c_discharge_orders/:id' => 'admin/one_c7_connectors#discharge', :as => :discharge
 end
